74b39394b2eae433ed20ffa6d5aafcea8a2da5c1
alvinnyen <alvinyen219@gmail.com>
2021-03-01 01:24:33 +0800

    handle basic mouse events

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import rough from "roughjs/bundled/rough.esm.js";
 
 const generator = rough.generator();
 
 function App() {
+  const [drawing, setDrawing] = useState(false);
+
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
     const roughCanvas = rough.canvas(canvas);
     const rect = generator.rectangle(10, 10, 100, 100);
     roughCanvas.draw(rect);
   });
+
+  const handleMouseDown = (event) => {
+    setDrawing(true);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
+
+    const { clientX, clientY } = event;
+    console.log(clientX, clientY);
+  };
+
+  const handleMouseUp = (event) => {
+    setDrawing(false);
+  };
 
   return (
     <div className="App">
@@ -20,9 +38,9 @@ function App() {
         style={{ backgroundColor: "blue" }}
         width={window.innerWidth}
         height={window.innerHeight}
-        // onMouseDown={handleMouseDown}
-        // onMouseMove={handleMouseMove}
-        // onMouseUp={handleMouseUp}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       >
         Canvas
       </canvas>
