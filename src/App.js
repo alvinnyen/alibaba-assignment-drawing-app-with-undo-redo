5bcdd5fbf584cb4ebf94c41fe9bc3ca39f3c3cbb
alvinnyen <alvinyen219@gmail.com>
2021-03-01 01:05:12 +0800

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useLayoutEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  useLayoutEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "green";
+    context.fillRect(10, 10, 150, 100);
+  });
+
+  return (
+    <div className="App">
+      <canvas
+        id="canvas"
+        style={{ backgroundColor: "blue" }}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        // onMouseDown={handleMouseDown}
+        // onMouseMove={handleMouseMove}
+        // onMouseUp={handleMouseUp}
+      >
+        Canvas
+      </canvas>
+    </div>
+  );
 }
 
 export default App;
